7c787cd85bf24f454e11231e8db6f70ffbb806a0
Allan Debastiani <allandeba@yahoo.com.br>
2023-11-22 21:47:50 -0300

    feat ItemModelTest

---
+++ iBudget.UnitTest/Models/ItemModelTest.cs
@@ -0,0 +1,78 @@
+using iBudget.Framework;
+using iBudget.Models;
+using Microsoft.AspNetCore.Http;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace iBudget.UnitTest.Models
+{
+    public class ItemModelTest
+    {
+        private FormFile GetFormFile(string nomeItem, string nomeArquivo)
+        {
+            Image image = new Image<Rgba32>(100, 100);
+            MemoryStream memoryStream = new();
+            image.Save(memoryStream, new PngEncoder());
+            FormFile formFile = new FormFile(memoryStream, 0, memoryStream.Length, nomeItem, nomeArquivo)
+            {
+                Headers = new HeaderDictionary(),
+                ContentType = "image/png"
+            };
+
+            return formFile;
+        }
+
+        private void FetchImageList(int numImages, List<IFormFile> imageFiles)
+        {
+            for (int i = 0; i < numImages; i++)
+            {
+                string nomeItem = "Item_" + i.ToString();
+                string nomeArquivo = "File_" + i.ToString();
+                imageFiles.Add(GetFormFile(nomeItem, nomeArquivo));
+            }
+        }
+
+        [Fact]
+        public void ShouldSetLastAddedImageAsMainImage()
+        {
+            ItemModel item = new();
+            FetchImageList(3, item.ImageFiles);
+
+            item.SetItemImageList();
+            item.SetDefaultImage(item.ImageFiles.Last().FileName);
+
+            ItemImageModel itemImage = item.ItemImageList.Find(i => i.Main);
+            Assert.NotNull(itemImage);
+
+            bool isLastImageDefault = itemImage.FileName == item.ImageFiles.Last().FileName;
+            Assert.True(isLastImageDefault);
+        }
+
+        [Fact]
+        public void ShouldNotHaveMainImage()
+        {
+            ItemModel item = new();
+            FetchImageList(3, item.ImageFiles);
+
+            item.SetItemImageList();
+            item.SetDefaultImage(SelectDefault.Nenhum.ToString());
+
+            ItemImageModel itemImage = item.ItemImageList.Find(i => i.Main);
+            Assert.Null(itemImage);
+        }
+
+        [Fact]
+        public void ShouldReturnMainImage()
+        {
+            ItemModel item = new();
+            FetchImageList(3, item.ImageFiles);
+
+            item.SetItemImageList();
+            item.SetDefaultImage(item.ImageFiles.First().FileName);
+
+            ItemImageModel itemImage = item.GetMainImage();
+            Assert.Equal(itemImage.FileName, item.ImageFiles.First().FileName);
+        }
+    }
+}
