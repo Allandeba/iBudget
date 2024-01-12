c899926e7a7904143afe80dfed5f4f8f5ecccee8
Allan Debastiani <allandeba@yahoo.com.br>
2024-01-12 16:15:29 -0300

    refactor Views.SearchBox implemented partialView

---
+++ Models/SearchBoxItem.cs
@@ -0,0 +1,8 @@
+namespace iBudget.Models;
+
+public class SearchBoxItem
+{
+    public string Action { get; set; }
+    public string ControllerName { get; set; }
+    public string SearchValue { get; set; }
+}
