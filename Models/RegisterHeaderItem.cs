ea1bee4ed5350ce2c2621af594956079476c77a1
Allan Debastiani <allandeba@yahoo.com.br>
2024-01-12 16:23:57 -0300

    refactor Views.Header implemented partialView

---
+++ Models/RegisterHeaderItem.cs
@@ -0,0 +1,8 @@
+namespace iBudget.Models;
+
+public class RegisterHeaderItem
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Action { get; set; }
+}
