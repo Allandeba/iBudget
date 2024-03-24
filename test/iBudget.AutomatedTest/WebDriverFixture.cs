1b7553a70c8637ca9e44a65c573a0dcc658d68fe
Allan Debastiani <allandeba@yahoo.com.br>
2024-03-24 16:17:50 -0300

    refactor folder structure workflows
    after validating workflow needs to add the deploy job

--- test/iBudget.AutomatedTest/WebDriverFixture.cs
+++ test/iBudget.AutomatedTest/WebDriverFixture.cs
@@ -8,10 +8,14 @@ public class WebDriverFixture : IDisposable
 
     public WebDriverFixture()
     {
-        _driver = new ChromeDriver();
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox");
+        options.AddArguments("--disable-dev-shm-usage");
+        options.AddArguments("--headless");
+        _driver = new ChromeDriver(options);
         _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(1000);
         _driver.Manage().Window.Maximize();
-        _driver.Navigate().GoToUrl("http://localhost:5105");
+        // _driver.Navigate().GoToUrl("http://localhost:5105");
     }
 
     public void Dispose()
