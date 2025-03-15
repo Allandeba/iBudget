8e238588892a96ccc6dff96eb278ed88a741d3e3
Allan Debastiani <allandeba@yahoo.com.br>
2025-03-14 21:17:07 -0300

    fix: password hash for default user

--- src/Framework/Cryptography.cs
+++ src/Framework/Cryptography.cs
@@ -11,10 +11,11 @@ public class Cryptography
         if (text.IsNullOrEmpty())
             throw new Exception("Cryptography.GetHash() must not be null or empty");
 
-        var sha256 = SHA256.Create();
-        var base64 = Encoding.Default.GetBytes(text);
-        var hashText = sha256.ComputeHash(base64);
-
-        return Convert.ToBase64String(hashText);
+        using (var sha256 = SHA256.Create())
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(text);
+            var hashBytes = sha256.ComputeHash(inputBytes);
+            return Convert.ToBase64String(hashBytes);
+        }
     }
 }
